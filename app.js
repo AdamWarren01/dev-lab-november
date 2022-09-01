f2e24f9870abcdbd9319ceef217a64e4d84a3731
Warren <awarren7@una.edu>
2022-09-01 13:21:53 -0500

    Added index.html and minor fixes

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const path = require('node:path')
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Node/Express/Heroku')
+
+
+  //res.send('Hello World from Node/Express/Heroku')
+
+  res.sendFile(
+    path.join(
+      __dirname,
+     "index.html"
+      )
+    );
+
 })
 
 app.listen(process.env.PORT || 3000,
